d48c8d1966f104c63271ee8d58253eebd075bcfe
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-23 16:44:42 +0200

    Added state to App Component To dynamically display NewProject or NoProjectSelcted Component Passed handleStartAddProject logic to other components to be added to Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject/NewProject';
+import NoProjectSelected from './components/NoProjectSelected/NoProjectSelected';
 import ProjectsSidebar from './components/ProjectsSidebar/ProjectsSidebar';
 
 function App() {
+  // selecteProjectId undefined (not chaning state), null (adding new project)
+  const [projectsState, setProjectsState] = useState({
+    selecteProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selecteProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selecteProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selecteProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
