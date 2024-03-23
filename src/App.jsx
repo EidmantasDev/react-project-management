9fa9a8c83d590fae374ec97a258a598b3ac1b629
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-23 16:08:09 +0200

    Added two new components Added NewProject component to the App component Created Input component so it would be easier to style and don't have to repeat Created a NewProject component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import ProjectsSidebar from './ProjectsSidebar/ProjectsSidebar';
+import NewProject from './components/NewProject/NewProject';
+import ProjectsSidebar from './components/ProjectsSidebar/ProjectsSidebar';
 
 function App() {
   return (
-    <main className='h-screen my-8'>
+    <main className='h-screen my-8 flex gap-8'>
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
